3eb41ddf526add8d1dfe13f28b2b6d0611f73987
lumenblacksquade <125724435+lumenblacksquade@users.noreply.github.com>
2023-02-18 12:58:09 +0530

    Add files via upload
    v2

---
+++ home.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<style>
+    .container-outer{
+        display: flex;
+        justify-content: space-around;
+    }
+</style>
+<body>
+
+    <nav>
+        <div class='container-outer'>
+        <div><button class="btn btn-primary" style="margin-top: 20px;">Event Management</button></div>
+        <div><input class="form-control" type="text" style="margin-top: 20px; width: 800px" placeholder="Search Events"></div>
+        <div><button class="btn btn-primary" style="margin-top: 20px;">Booking</button></div>
+        
+        </div>
+      
+    </nav>
+    
+
+</body>
+</html>
